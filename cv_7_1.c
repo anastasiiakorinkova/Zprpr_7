a9505838261a3933f1757e68a7beef58f01895b9
anastasiiakorinkova <114064846+anastasiiakorinkova@users.noreply.github.com>
2022-11-07 10:18:47 +0200

    Create cv_7_1.c

---
+++ cv_7_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>  
+  
+void sucet(int, int, int*);  
+  
+int main()  
+{  
+    int a, b, s;  
+  
+    printf("Zadaj dve cisla oddelene medzerou:\n");  
+    scanf("%d%d", &a, &b);  
+  
+    sucet(a, b, &s);  
+  
+    printf("Sucet cisel je: %d\n", s);  
+  
+    return 0;  
+}  
+  
+void sucet(int x, int y, int *z)  
+{  
+    *z = x + y;  
+}
