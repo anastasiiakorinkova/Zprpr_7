4098267e3ad2baea5b99a18e2d0f16cfecdf883b
anastasiiakorinkova <114064846+anastasiiakorinkova@users.noreply.github.com>
2022-11-07 15:12:46 +0200

    Update cv_7_2.c

--- cv_7_2.c
+++ cv_7_2.c
@@ -1,47 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
-void createRow(int cis, int *nums)
-{
-    if (!cis % 2)
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            nums[i] = 32 + (i * 2);
-            if (i == 0)
-            {
-                nums[i] = 32;
+void vytvorPole(int cis, int* nums) {
+    if (cis % 2) {
+        for (int i = 0; i < 5; i++) {
+            nums[i] = 73 + (i * 2);
+            if (i == 0) {
+                nums[i] = 73;
             }
         }
+        
     }
-    else
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            nums[i] = 73 + (i * 2);
-            if (i == 0)
-            {
-                nums[i] = 73;
+    else {
+        for (int i = 0; i < 5; i++) {
+            nums[i] = 32 + (i * 2);
+            if (i == 0) {
+                nums[i] = 32;
             }
         }
     }
 }
 
-int main()
-{
-    int num;
+int main() {
+    int cis;
 
-    printf("Zadajte cislo: \n");
-    scanf("%d", &num);
+    printf("Zadajte cislo \n");
+    scanf("%d", &cis);
 
-    int *numb;
-    numb = (int *)malloc(5 * sizeof(int));
+    int* nums;
+    nums = (int*)malloc(5 * sizeof(int));
 
-    createRow(num, numb);
+    vytvorPole(cis, nums);
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d\n", numb[i]);
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", nums[i]);
     }
 
     return 0;
