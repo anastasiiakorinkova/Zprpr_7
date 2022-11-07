07b6c0601cdbc29ed893ad972ca996c85da59454
anastasiiakorinkova <114064846+anastasiiakorinkova@users.noreply.github.com>
2022-11-07 14:46:21 +0100

    Update cv_7_2.c

--- cv_7_2.c
+++ cv_7_2.c
@@ -1,39 +1,48 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 
-void vytvorPole(int cis, int* nums) {
+void vytvorPole(int cis, int* arr) {
+
+
     if (cis % 2) {
         for (int i = 0; i < 5; i++) {
-            nums[i] = 73 + (i * 2);
-            if (i == 0) {
-                nums[i] = 73;
-            }
+
+            arr[i] = 73 + (i * 2);
         }
-        
+
     }
     else {
+
         for (int i = 0; i < 5; i++) {
-            nums[i] = 32 + (i * 2);
-            if (i == 0) {
-                nums[i] = 32;
-            }
+            arr[i] = 32 + (i * 2);
+
         }
     }
 }
+
+
 
 int main() {
     int cis;
 
     printf("Zadajte cislo \n");
-    scanf("%d", &cis);
+    scanf_s("%d", &cis);
 
-    int* nums;
-    nums = (int*)malloc(5 * sizeof(int));
+    int* arr;
+    arr = (int*)malloc(5 * sizeof(int));
 
-    vytvorPole(cis, nums);
+    vytvorPole(cis, arr);
 
-    for (int i = 0; i < 5; i++) {
-        printf("%d\n", nums[i]);
+    if (cis == 0) {
+        printf("Cislo nie je ani parne, ani neparne\n");
+
+    }
+
+    else {
+        for (int i = 0; i < 5; i++) {
+
+            printf("%d\n", arr[i]);
+        }
     }
 
     return 0;
